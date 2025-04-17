9c97e33f3eae8cd97d77762dc220d69f0ab59d76
lystosas <lystosas@gmail.com>
2025-04-16 22:39:39 -0500

    primer git

---
+++ pos-saas/src/test/java/com/businessgroup/pos_saas/PosSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessgroup.pos_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
